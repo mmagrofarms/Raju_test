564ca20fe4065943e4a2296191762941d882a419
M&M Agro Farms <shiva.balla007@gmail.com>
2025-07-29 18:13:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to M&M</title>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+    <link rel="stylesheet" href="style.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+   </head>
+<body>
+
+    <!-- navbar -->
+
+    <div class="container-fluid p-0" >
+        <!--first chaild-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+            <div class="container-fluid">
+                <img src="./images/logo.png" alt="" class="logo">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+        <!-- main image -->
+
+
+        
+
+        <section id="hero">
+            <div class="container-fluid">
+                <div class="row">
+                    
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div id="heropera">
+                            <H1> Highlights of our Farm </H1>
+                            <br><br>
+                            <h5>1. 100% Natural growing method.</h5>
+                            <h5>2. 100% Organic feed given to flock from our Chemical-free cultivated crops</h5>
+                            <h5>3. No Hormoner, Antibiotics and Non- injectable.</h5>
+                            <h5>4. 100% Healthy, Tasty and Hygiene meat (goat, Sheep and Hen)</h5>
+                            <h5>5. We raise only Orginal breads-Stronger and Healthier</h5>
+                            <h5>6. 100% Pure Desi eggs</h5>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <!-- <H1 id="ab">About Us:</H1> -->
+                        <img src="./images/frame1.png" alt="" class="mainimg">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="Benefit">
+            <div class="container-fluid">
+                <div class="row">                        
+                    <H1> Benefits of our Products</H1>                    
+                </div>
+            </div>
+        </section>
+
+
+        <section id="orderplace">
+            <div class="container">
+                <div class="row align-items-start">
+                    <div class="col">
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./images/logo.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    </div>
+                    <div class="col">
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./images/logo.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col">
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./images/logo.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+
+
+
+
+
+        
+        <!-- last chail footer -->
+        <div class = "bg-warning p-3 text-center">
+            <p>All rights reserved Designed by Ganesh-2025</P>
+        </div> 
+
+
+    </div>
+
+
+
+
+
+
+      
+  
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" ></script>
+    
+</body>
+</html>
