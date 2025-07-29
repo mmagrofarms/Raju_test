4a4f65defce4302dbf3e9079884ed4d279acc4d2
M&M Agro Farms <shiva.balla007@gmail.com>
2025-07-29 18:56:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="Order.html" class="btn btn-primary">Order Here</a>
+                                <a href="order.html" class="btn btn-primary">Order Here</a>
                             </div>
                         </div>
                     </div>
